09d89863b5afa7c814eacbac7b70e927f90e208d
Davis <davis.sermons@gmail.com>
2018-07-22 14:30:59 +0300

    feat:add BooksList, BooksSearch, Book components

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React from 'react';
 // import * as BooksAPI from './BooksAPI'
 import './App.css'
 
---
+++ src/BooksList.js
@@ -0,0 +1,7 @@
+import React, { Component } from 'react';
+
+class BooksList extends Component {
+
+}
+
+export default BooksList;
